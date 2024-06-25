e912f7a0fd7a0dd683be2f7da34d57d9c21b5ed0
MihalIvanych <78982503+MihalIvanych@users.noreply.github.com>
2024-06-25 13:57:03 +0600

    (fix): reserve link is target blank

--- resources/views/partials/reserve_button.blade.php
+++ resources/views/partials/reserve_button.blade.php
@@ -1,1 +1,1 @@
-<a href="{{__('contacts.reserve_link')}}" class="c-button">{!! __('app.reserve_experience_button') !!}</a>
+<a href="{{__('contacts.reserve_link')}}" class="c-button" target="_blank">{!! __('app.reserve_experience_button') !!}</a>
