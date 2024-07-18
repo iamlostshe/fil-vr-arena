7e7d6b80a09eee7f17ac95b62150ddac5c7d6e5c
monkeyfhtagn <monkey.fhtagn@gmail.com>
2024-07-18 19:17:21 +0600

    обертка блоками для онлайн-бронирования

--- resources/views/pages/online_book.blade.php
+++ resources/views/pages/online_book.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
-<div id="resova-wrapper"></div>
+<section class="c-section" id="section-online-booking">
+    <div class="c-layout">
+        <div id="resova-wrapper"></div>
+    </div>
+</section>
 <script>(function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
