dd139883966d8e607d6e5f04c7cf3e1582e87702
fraq <zuev.p.v@gmail.com>
2024-11-11 08:06:31 +0000

    feat(main): Виджет на странице

--- resources/views/pages/online_book.blade.php
+++ resources/views/pages/online_book.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.app')
-<div id="resova-wrapper"></div>
-<script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "https://vrarena.resova.co.uk/widget";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'resova-pi'));
-</script>
+
+@section('page')
+        <div id="resova-wrapper"></div>
+        <script>(function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "https://vrarena.resova.co.uk/widget";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'resova-pi'));
+        </script>
+@endsection
