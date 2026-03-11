7d0f56ec1b8fe61e5839693f220f6f8f5a63b622
Иван <i@iamlostshe.ru>
2026-03-12 00:18:07 +0400

    fuck: nova

--- config/app.php
+++ config/app.php
@@ -168,8 +168,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\MetaTagsServiceProvider::class,
-        App\Providers\NovaServiceProvider::class,
+        // App\Providers\MetaTagsServiceProvider::class,
+        // App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ])->toArray(),
